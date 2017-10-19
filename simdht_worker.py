78144a194bac4e536908f9a39a0773b204160731
dipoo <120826531@qq.com>
2017-10-19 14:28:26 +0800

    Update simdht_worker.py

--- simdht_worker.py
+++ simdht_worker.py
@@ -61,17 +61,17 @@ def get_extension(name):
 def get_category(ext):
     ext = ext + '.'
     cats = {
-        u'影视': '.avi.mp4.rmvb.m2ts.wmv.mkv.flv.qmv.rm.mov.vob.asf.3gp.mpg.mpeg.m4v.f4v.',
-        u'图像': '.jpg.bmp.jpeg.png.gif.tiff.',
-        u'文档书籍': '.pdf.isz.chm.txt.epub.bc!.doc.ppt.',
-        u'音乐': '.mp3.ape.wav.dts.mdf.flac.',
-        u'压缩文件': '.zip.rar.7z.tar.gz.iso.dmg.pkg.',
-        u'安装包': '.exe.app.msi.apk.'
+        u'1': '.avi.mp4.rmvb.m2ts.wmv.mkv.flv.qmv.rm.mov.vob.asf.3gp.mpg.mpeg.m4v.f4v.',
+        u'2': '.jpg.bmp.jpeg.png.gif.tiff.',
+        u'3': '.pdf.isz.chm.txt.epub.bc!.doc.ppt.',
+        u'4': '.mp3.ape.wav.dts.mdf.flac.',
+        u'5': '.zip.rar.7z.tar.gz.iso.dmg.pkg.',
+        u'6': '.exe.app.msi.apk.'
     }
     for k, v in cats.iteritems():
         if ext in v:
             return k
-    return u'其他'
+    return u'10'
 
 def get_detail(y):
     if y.get('files'):
